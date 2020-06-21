9718b3cbb147fbb0674166bcbfd47e1f748c0c5e
Michel Sievers <michel@easy-target.eu>
2020-06-22 00:09:41 +0200

    Add first impl of pulsating volumetric spheres

--- src/main/kotlin/eu/ezytaget/processing/kotlin_template/PApplet.kt
+++ src/main/kotlin/eu/ezytaget/processing/kotlin_template/PApplet.kt
@@ -2,12 +2,21 @@ package eu.ezytaget.processing.kotlin_template
 
 import eu.ezytaget.processing.kotlin_template.palettes.DuskPalette
 import processing.core.PConstants
+import processing.core.PVector
 
 class PApplet : processing.core.PApplet() {
 
     private val backgroundDrawer = BackgroundDrawer(DuskPalette(), alpha = 0.01f)
 
     private var waitingForClickToDraw = false
+
+    private var spheres: MutableList<Sphere> = mutableListOf()
+
+    private var minNumberOfSpheres = 2
+
+    private var maxNumberOfSpheres = 16
+
+    private var radiusFactor = DESIRED_RADIUS_FACTOR
 
     override fun settings() {
         if (FULL_SCREEN) {
@@ -23,6 +32,7 @@ class PApplet : processing.core.PApplet() {
         clearFrame()
         noCursor()
         lights()
+        initSpheres()
     }
 
     override fun draw() {
@@ -34,17 +44,19 @@ class PApplet : processing.core.PApplet() {
             backgroundDrawer.draw(pApplet = this, alpha = 0.1f)
         }
 
-        val lineLength = height / 4f
+        updateRadiusFactor()
 
-        stroke(0f, 0f, 1f, 0.1f)
+        translate(width / 2f, height / 2f)
 
-        repeat((0 .. 1024).count()) {
-            bentLine(random(width.toFloat()), random(height.toFloat()), lineLength)
+        val baseRotation = ((millis() % 10_000) / 10_000f) * TWO_PI
+
+        spheres.forEach {
+            pushMatrix()
+            rotateY(baseRotation * it.rotationSpeed)
+            stroke(it.colorValue1, it.colorValue2, it.colorValue3, it.alpha)
+            noiseSphere(radius = it.radius * radiusFactor, randomSeed =  it.randomSeed)
+            popMatrix()
         }
-
-        stroke(0.5f, 1f, 1f, 1f)
-        bentLine(mouseX.toFloat(), mouseY.toFloat(), lineLength)
-
 
         if (CLICK_TO_DRAW) {
             waitingForClickToDraw = true
@@ -53,6 +65,7 @@ class PApplet : processing.core.PApplet() {
 
     override fun keyPressed() {
         when (key) {
+            ' ' -> radiusFactorVelocity = 0.01f
         }
     }
 
@@ -63,37 +76,73 @@ class PApplet : processing.core.PApplet() {
         )
     }
 
-    private fun bentLine(centerX: Float, centerY: Float, zeroLength: Float) {
-        pushMatrix()
 
-        val relativeDistanceToXCenter = (centerX - (width / 2f)) / (width / 2f)
-        val normalizedDistanceToXCenter = abs(relativeDistanceToXCenter)
-        val length = zeroLength + ((height - zeroLength) * normalizedDistanceToXCenter)
+    private fun initSpheres() {
+        spheres = mutableListOf()
+        val numberOfSpheres = random(minNumberOfSpheres.toFloat(), (maxNumberOfSpheres + 1).toFloat()).toInt()
+        val minRadius = width / 16f
+        val maxRadius = width / 2f
 
-        val relativeDistanceToYCenter = (centerY - (height / 2f)) / (height / 2f)
-        val rotationSign = relativeDistanceToXCenter
-        translate(centerX, centerY)
-        rotate(relativeDistanceToYCenter * relativeDistanceToXCenter)
-        translate(0f,- (length / 2f))
-        line(
-                0f,
-                0f,
-                0f,
-                length
-        )
-        popMatrix()
+        val minRotationSpeed = 2f
+        val maxRotationSpeed = 8f
+        val colorValue1 = random(1f)
+
+        repeat((0..numberOfSpheres).count()) {
+            val radius = if (it == 0) {
+                maxRadius
+            } else {
+                random(minRadius, maxRadius)
+            }
+
+            val sphere = Sphere(
+                    radius = radius,
+                    randomSeed = random(2000f).toLong(),
+                    rotationSpeed = random(minRotationSpeed, maxRotationSpeed),
+                    colorValue1 = colorValue1,
+                    colorValue3 = 1f,
+                    colorValue2 = 1f,
+                    alpha = 1f
+            )
+            spheres.plusAssign(sphere)
+        }
+    }
+
+    private fun noiseSphere(radius: Float, randomSeed: Long, numberOfPoints: Int = 4096 * 2) {
+        randomSeed(randomSeed)
+        repeat((0..numberOfPoints).count()) {
+            val randomVector = PVector(
+                    random(-100f, 100f),
+                    random(-100f, 100f),
+                    random(-100f, 100f)
+            ).setMag(radius)
+            point(randomVector.x, randomVector.y, randomVector.z)
+        }
+    }
+
+    private var radiusFactorVelocity = 0f
+
+    private fun updateRadiusFactor() {
+        val delta = radiusFactor - DESIRED_RADIUS_FACTOR
+        if (delta > RADIUS_FACTOR_TOLERANCE) {
+            radiusFactorVelocity -= RADIUS_FACTOR_PULL
+        }
+
+        radiusFactor += radiusFactorVelocity
     }
 
     companion object {
         private const val CLICK_TO_DRAW = false
-        private const val FULL_SCREEN = true
+        private const val FULL_SCREEN = false
         private const val WIDTH = 1400
         private const val HEIGHT = 900
-        private const val RENDERER = PConstants.P2D
+        private const val RENDERER = PConstants.P3D
         private const val COLOR_MODE = PConstants.HSB
         private const val MAX_COLOR_VALUE = 1f
         private const val FRAME_RATE = 60f
         private const val DRAW_BACKGROUND_ON_DRAW = true
+        private const val DESIRED_RADIUS_FACTOR = 1f
+        private const val RADIUS_FACTOR_TOLERANCE = 0.01f
+        private const val RADIUS_FACTOR_PULL = 0.01f
 
         fun runInstance() {
             val instance = PApplet()
---
+++ src/main/kotlin/eu/ezytaget/processing/kotlin_template/Sphere.kt
@@ -0,0 +1,11 @@
+package eu.ezytaget.processing.kotlin_template
+
+data class Sphere(
+        val radius: Float,
+        val randomSeed: Long,
+        val rotationSpeed: Float,
+        val colorValue1: Float,
+        val colorValue3: Float,
+        val colorValue2: Float,
+        val alpha: Float
+)
