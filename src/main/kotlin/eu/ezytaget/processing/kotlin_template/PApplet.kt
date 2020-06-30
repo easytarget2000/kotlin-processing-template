ba3b831e08fe9621057f78f2725643255a0469e5
Michel Sievers <michel@easy-target.eu>
2020-07-01 00:16:28 +0200

    Add simple starfield

--- src/main/kotlin/eu/ezytaget/processing/kotlin_template/PApplet.kt
+++ src/main/kotlin/eu/ezytaget/processing/kotlin_template/PApplet.kt
@@ -10,21 +10,21 @@ class PApplet : processing.core.PApplet() {
 
     private val clapper = Clapper()
 
-    private var lastBeatIntervalCount = 0
-
     private val backgroundDrawer = BackgroundDrawer(DuskPalette(), alpha = 0.01f)
 
     private var waitingForClickToDraw = false
 
-    private var spheres: MutableList<Sphere> = mutableListOf()
+    private var drawBackgroundOnDraw = true
 
-    private var minNumberOfSpheres = 2
+    private var backgroundAlpha = 1f
 
-    private var maxNumberOfSpheres = 16
+    private lateinit var starPositions: List<PVector>
 
-    private var radiusFactor = DESIRED_RADIUS_FACTOR
+    private var numberOfStars = 700
 
-    private var drawNoiseSpheres = false
+    private var maxStarRadius = 8f
+
+    private var speed = 40f
 
     override fun settings() {
         if (FULL_SCREEN) {
@@ -34,14 +34,23 @@ class PApplet : processing.core.PApplet() {
         }
     }
 
+    private val maxStarZ: Float
+        get() = max(width, height).toFloat()
+
     override fun setup() {
         frameRate(FRAME_RATE)
         colorMode(COLOR_MODE, MAX_COLOR_VALUE)
         clearFrame()
         noCursor()
         lights()
-        initSpheres()
+        clapper.bpm = 128f
         clapper.start()
+
+        val starPositions = mutableListOf<PVector>()
+        repeat((0 until numberOfStars).count()) {
+            starPositions.add(nextRandomPosition())
+        }
+        this.starPositions = starPositions
     }
 
     override fun draw() {
@@ -49,35 +58,28 @@ class PApplet : processing.core.PApplet() {
             return
         }
 
-        if (DRAW_BACKGROUND_ON_DRAW) {
-            backgroundDrawer.draw(pApplet = this, alpha = 0.1f)
+        if (drawBackgroundOnDraw) {
+            backgroundDrawer.draw(pApplet = this, alpha = backgroundAlpha)
         }
 
-        if(clapper.update()) {
-            handleClapperValue()
-        }
-
-        updateRadiusFactor()
-
+        val width = width.toFloat()
+        val height = height.toFloat()
+        stroke(0.3f, 1f, 1f, 0.5f)
+        fill(1f)
         translate(width / 2f, height / 2f)
-
-        val baseRotation = ((millis() % 100_000) / 100_000f) * TWO_PI
-
-        noFill()
-
-        sphereDetail(16)
-
-        spheres.forEach {
-            pushMatrix()
-            rotateY(baseRotation * it.rotationSpeed)
-            stroke(it.colorValue1, it.colorValue2, it.colorValue3, it.alpha)
-            val radius = it.radius * radiusFactor
-            if (drawNoiseSpheres) {
-                noiseSphere(radius, randomSeed = it.randomSeed)
+        starPositions = starPositions.map {
+            drawStar(it, width, height)
+            val z = it.z - speed
+            if (z < 1f) {
+                nextRandomPosition(randomZ = false)
             } else {
-                sphere(radius)
+                PVector(
+                        it.x,
+                        it.y,
+                        z
+                )
             }
-            popMatrix()
+
         }
 
         if (CLICK_TO_DRAW) {
@@ -85,12 +87,31 @@ class PApplet : processing.core.PApplet() {
         }
     }
 
+    private fun drawStar(position: PVector, width: Float, height: Float) {
+        val depthAdjustedX = map(position.x / position.z, 0f, 1f, 0f, width)
+        val depthAdjustedY = map(position.y / position.z, 0f, 1f, 0f, height)
+        val radius = map(position.z, 0f, maxStarZ, maxStarRadius, 0f)
+        ellipse(depthAdjustedX, depthAdjustedY, radius, radius)
+    }
+
     override fun keyPressed() {
         when (key) {
+            'x' ->
+                clearFrame()
             ' ' ->
                 clapper.tapBpm()
         }
     }
+
+    private fun nextRandomPosition(randomZ: Boolean = true) = PVector(
+            random(-width / 2f, width / 2f),
+            random(-height / 2f, height / 2f),
+            if (randomZ) {
+                random(maxStarZ)
+            } else {
+                maxStarZ
+            }
+    )
 
     private fun clearFrame() {
         backgroundDrawer.draw(
@@ -99,114 +120,18 @@ class PApplet : processing.core.PApplet() {
         )
     }
 
-    private fun toggleDrawStyle() {
-        drawNoiseSpheres = !drawNoiseSpheres
-    }
-
-    private fun initSpheres() {
-        spheres = mutableListOf()
-        val numberOfSpheres = random(minNumberOfSpheres.toFloat(), (maxNumberOfSpheres + 1).toFloat()).toInt()
-        val minRadius = width / 16f
-        val maxRadius = width / 2f
-
-        val minRotationSpeed = 2f
-        val maxRotationSpeed = 8f
-        val colorValue1 = random(1f)
-
-        repeat((0..numberOfSpheres).count()) {
-            val radius = if (it == 0) {
-                maxRadius
-            } else {
-                random(minRadius, maxRadius)
-            }
-
-            val sphere = Sphere(
-                    radius = radius,
-                    randomSeed = random(2000f).toLong(),
-                    rotationSpeed = random(minRotationSpeed, maxRotationSpeed),
-                    colorValue1 = colorValue1,
-                    colorValue3 = 1f,
-                    colorValue2 = 1f,
-                    alpha = 1f
-            )
-            spheres.plusAssign(sphere)
-        }
-    }
-
-    private fun handleClapperValue() {
-        val intervalNumbers = clapper.intervalNumbers
-        if (lastBeatIntervalCount != intervalNumbers.getValue(BeatInterval.TwoWhole)) {
-
-//            val random = random(1f)
-//            when {
-//                random < 0.2f -> {
-//                    bounce()
-//                }
-//                random < 0.4f -> {
-//                    initSpheres()
-//                }
-//                random < 0.6f -> {
-//
-//                }
-//            }
-
-            maybe { initSpheres() }
-            maybe { toggleDrawStyle() }
-
-            bounce()
-
-            lastBeatIntervalCount = intervalNumbers.getValue(BeatInterval.TwoWhole)
-        }
-    }
-
-    private fun maybe(probability: Float = 0.5f, lambda: (() -> Unit)) {
-        if (random(1f) < probability) {
-            lambda()
-        }
-    }
-
-    private fun noiseSphere(radius: Float, randomSeed: Long, numberOfPoints: Int = 4096 * 2) {
-        randomSeed(randomSeed)
-        repeat((0..numberOfPoints).count()) {
-            val randomVector = PVector(
-                    random(-100f, 100f),
-                    random(-100f, 100f),
-                    random(-100f, 100f)
-            ).setMag(radius)
-            point(randomVector.x, randomVector.y, randomVector.z)
-        }
-    }
-
-    private var radiusFactorVelocity = 0f
-
-    private fun updateRadiusFactor() {
-
-        if (radiusFactor < DESIRED_RADIUS_FACTOR) {
-            radiusFactor = DESIRED_RADIUS_FACTOR
-        } else if (radiusFactor > DESIRED_RADIUS_FACTOR) {
-            radiusFactorVelocity -= RADIUS_FACTOR_PULL
-        }
-
-        radiusFactor += radiusFactorVelocity
-    }
-
-    private fun bounce() {
-        radiusFactorVelocity = 0.05f
-    }
-
     companion object {
-        private const val CLICK_TO_DRAW = false
-        private const val FULL_SCREEN = false
+        private const val CLICK_TO_DRAW = true
+        private const val FULL_SCREEN = true
         private const val WIDTH = 1400
         private const val HEIGHT = 900
-        private const val RENDERER = PConstants.P3D
+        private const val RENDERER = PConstants.P2D
         private const val COLOR_MODE = PConstants.HSB
         private const val MAX_COLOR_VALUE = 1f
         private const val FRAME_RATE = 60f
-        private const val DRAW_BACKGROUND_ON_DRAW = true
         private const val DESIRED_RADIUS_FACTOR = 1f
         private const val RADIUS_FACTOR_TOLERANCE = 0.01f
-        private const val RADIUS_FACTOR_PULL = 0.01f
+        private const val RADIUS_FACTOR_PULL = 0.001f
 
         fun runInstance() {
             val instance = PApplet()
