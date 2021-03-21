c7a499902caad30b58a27a7aa03f2c9db8820b92
Michel Sievers <michel@easy-target.eu>
2021-03-21 23:47:22 +0100

    feat: make endless tunnel with simple ellipses

--- src/main/kotlin/eu/ezytaget/processing/kotlin_template/realms/tunnel/TunnelReam.kt
+++ src/main/kotlin/eu/ezytaget/processing/kotlin_template/realms/tunnel/TunnelReam.kt
@@ -1,6 +1,7 @@
 package eu.ezytaget.processing.kotlin_template.realms.tunnel
 
 import eu.ezytaget.processing.kotlin_template.realms.Realm
+import processing.core.PApplet
 import processing.core.PConstants.CENTER
 import processing.core.PGraphics
 import kotlin.random.Random
@@ -17,17 +18,42 @@ class TunnelReam(random: Random = Random.Default): Realm(random) {
 
     var alpha = 1f
 
+    var segmentWidth = 40f
+
+    var numberOfSegments = 64
+
+    var relativeSegmentOffset = 0f
+
+    var velocity = 0.01f
+
+    override fun update(pApplet: PApplet) {
+        super.update(pApplet)
+        relativeSegmentOffset += velocity
+        if (relativeSegmentOffset > 1f) {
+            relativeSegmentOffset = 0f
+        }
+    }
+
     override fun drawIn(pGraphics: PGraphics) {
         super.drawIn(pGraphics)
         beginDraw(pGraphics)
+
+        val startZ = segmentWidth * (numberOfSegments / 3f) + (relativeSegmentOffset * segmentWidth)
+        pGraphics.translate(pGraphics.width / 2f, pGraphics.height / 2f, startZ)
 
         pGraphics.stroke(colorValue1, colorValue2, colorValue3, alpha)
         pGraphics.noFill()
         pGraphics.ellipseMode(CENTER)
 
-        pGraphics.ellipse(0f, 0f, tunnelDiameter, tunnelDiameter)
+        (0 until numberOfSegments).forEach { _ ->
+            drawSegment(pGraphics)
+            pGraphics.translate(0f, 0f, -segmentWidth)
+        }
 
         endDraw(pGraphics)
     }
 
+    private fun drawSegment(pGraphics: PGraphics) {
+        pGraphics.ellipse(0f, 0f, tunnelDiameter, tunnelDiameter)
+    }
 }
